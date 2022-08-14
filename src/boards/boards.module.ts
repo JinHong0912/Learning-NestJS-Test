f3e887cca38b09a4fd4b98a9c0e1c985140449e7
JinHong <jin.ryu0912@gmail.com>
2022-08-14 21:03:29 +0900

    update : Repository add

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Repository])],
   controllers: [BoardsController],
   providers: [BoardsService],
-
 })
 export class BoardsModule {}
